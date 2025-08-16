b83c4286bb01686a93f3ac844fdc74fc59e69f8a
darilsobze <darilsobze13@icloud.com>
2025-08-16 23:53:34 +0200

    Labor1: I will be a Googler!!!

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main( String[] args){
 
         System.out.println("Welcome to the Amazon Junior Software Dev Masterclass !! ");
+        System.out.println("I'm a Programmer and will be a Googler");
+
     }
 }
